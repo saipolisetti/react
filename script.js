2695d125cadba19447a7c7d919d7db66455db5fc
saipolisetti <saipolisetti700@gmail.com>
2019-07-11 13:27:17 +0530

    adding additional statements to script file

--- script.js
+++ script.js
@@ -2,14 +2,22 @@ function Student() {
     this.name = 'John';
     this.gender = 'M';
 }
-
+Student.prototype={age:23};
 var studObj = new Student();
-
-console.log(Student.prototype); // object
+var studObj1 = new Student();
+Student.prototype={age:24};
+var studObj2 = new Student();
+console.log( Student.prototype); // object
 console.log(studObj.prototype); // undefined
 console.log(studObj.__proto__); // object
-
+console.log(studObj.age);
+console.log(studObj1.age);
+console.log(studObj2.age);
 console.log(typeof Student.prototype); // object
 console.log(typeof studObj.__proto__); // object
 
 console.log(Student.prototype === studObj.__proto__ ); // true
+console.log(typeof Student);
+console.log( Student instanceof Function);
+console.log(studObj instanceof Object);
+console.log( Student instanceof Object);
